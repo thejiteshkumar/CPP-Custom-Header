b41957925720c4b8e5cd20169b99fe5cf8bc7b9c
Jitesh Kumar <60359722+dreagon19@users.noreply.github.com>
2021-11-04 15:22:13 +0530

    adding test.cpp temporally

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+#include "shortcuts.h"
+
+#define lli long long int
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+using namespace std;
+
+int main()
+{
+    fastio
+
+        if (check_LargePrime(7847777799997777555555))
+            cout<< "PRIME";
+        else 
+        cout << "NOT PRIME";
+
+    return 0;
+}
