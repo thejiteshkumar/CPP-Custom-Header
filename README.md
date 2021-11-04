6d7cf879cb1658d421dc2a24ea0819d979937ffb
Jitesh Kumar <60359722+dreagon19@users.noreply.github.com>
2021-11-05 01:06:16 +0530

    Added fibonacci and updated readme

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ Just include this header file
 
 - [Check Prime Numbers](#Check-Prime)
 - [Check Large Prime Numbers](#Check-Large-Prime-number)
+- [Factorial of a large Number](#Factorial-of-a-large-number)
+- [Nth Fibonacci Number](#Nth-Fibonacci-Number)
 
 
 
@@ -33,13 +35,39 @@ The various Functions included are:
 
     Sieve of Eratosthenes is used.
     
+    Returns a "boolean" result.
     
-    check_Prime(variable)
+    USE: bool prime = check_Prime(variable);
+
 ## Check Large Prime number
 
     This function works on a large prime numbers.
 
-    Use long long or long long int 
+    Use long long or long long int. 
+
+    Returns a "boolean" result.
+
+    USE: bool prime = check_LargePrime(variable);
+
+## Factorial of a large number
+
+    This function returns results in O(nlogn).
+
+    Returns a "vector<int>" as the answer produce is large and cannot be stored in any data type.
+
+    USE: vector<int> ans = factorialOfLargeNumber(variable);
+
+## Nth Fibonacci Number
+
+    This function returns results in O(n) and take O(1) constant space.
+
+    Returns nth fibonacci in "long long int" type.
+
+    USE: long long int fib = nthFibonacci(variables);
 
 
-    check_LargePrime(variable)
+
+
+
+    
+    
--- shortcuts.h
+++ shortcuts.h
@@ -5,6 +5,8 @@ This header file main purpose is to implement most famous and most used alorithm
 This header file includes :
     1. Prime Number Check 
     2. Large Prime Number Check
+    3. Factorial of Large Number
+    4. Nth Fibonacci Number
 
 
 
@@ -16,11 +18,15 @@ This header file includes :
 
 #include <bits/stdc++.h>
 #define lli long long int
+#define MODULO 1000000007
 
 using namespace std;
 
+//=============================================================================================
+
 
 // Uses Sieve of Eratosthenes and has O(N log (log N)) complexity with O(n) auxilary space.
+
 bool check_Prime(lli n)
 {
     vector<bool> isPrime(n + 1, true);
@@ -55,3 +61,68 @@ bool check_LargePrime(lli n)
     else
         return true;
 }
+
+//=============================================================================================
+
+
+// Class containing all factorial related problems
+class Factorial
+{
+public:
+    vector<int> factorial(int N)
+    {
+        vector<int> res = {1};
+
+        for (int i = 2; i <= N; i++)
+        {
+            int carry = 0;
+
+            for (int j = 0; j < res.size(); j++)
+            {
+                int prod = (res[j] * i) + carry;
+                res[j] = prod % 10;
+                carry = prod / 10;
+            }
+            while (carry)
+            {
+                res.push_back(carry % 10);
+                carry /= 10;
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+vector<int> factorialOfLargeNumber(int n){
+
+    Factorial obj;
+    vector<int> res = obj.factorial(n);
+
+    return res;
+}
+
+//====================================================================================
+
+//nth Fibonacci Number
+
+// time complexity O(n) and space as O(1) 
+// works great till n<1000
+
+lli nthFibonacci(lli n){
+    lli a = 0, b = 1, c;
+
+    if(n==0)
+        return a;
+    for (lli i = 2; i <= n;i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+
+
+
+
