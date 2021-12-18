bb2e1d63740d3c917d0531f16bb551cb1be329b7
Jitesh Kumar <60359722+dreagon19@users.noreply.github.com>
2021-12-18 21:59:00 +0530

    Added Trapping Rain Water Problem

--- shortcuts.h
+++ shortcuts.h
@@ -18,6 +18,7 @@ This header file includes :
     -Sort Element by frequency
     -Kadane's Algorithm (Largest Sum Contiguous Subarray)
     -Count pairs with given sum
+    -Trapping Rain Water
 
 
 
@@ -334,3 +335,35 @@ int getPairsCount(int arr[],int n,int k){
 
     return res;
 }
+
+// Trapping Rain Water Problem
+
+/**
+ * @brief Given an array arr[] of N non-negative integers representing the height of blocks. If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+ *
+ * @param Vector 
+ * @param n No of element 
+ * @return int how much rain water is trapped inside
+ */
+int trappedRainWater(vector<int> arr,int n){
+    int res =0;
+    int lmax[n], rmax[n];
+
+    lmax[0] = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        lmax[i] = max(arr[i], lmax[i - 1]);
+    }
+    rmax[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rmax[i] = max(arr[i], rmax[i + 1]);
+    }
+    for (int i = 1; i < n - 1; i++)
+    {
+        res += min(lmax[i], rmax[i]) - arr[i];
+    }
+    return res;
+}
+
